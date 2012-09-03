6dd78870497e5e201035f121849bca01785960d8
Jordan Komnick <jordan@komnick.com>
2012-09-02 21:22:54 -0400

    draws half circles
    don't know why it's not drawing full circles

--- src/HW01Part2App.cpp
+++ src/HW01Part2App.cpp
@@ -16,7 +16,8 @@ class HW01Part2App : public AppBasic {
 	void update();
 	void draw();
 	void prepareSettings(Settings* settings);
-	void HW01Part2App::drawCircle(uint8_t* pixels, int center_x, int center_y, int radius, Color8u color);
+	void drawCircle(uint8_t* pixels, int center_x, int center_y, int radius, Color8u color);
+	void drawSquare(uint8_t* pixels, int corner_x, int corner_y, int width, Color8u color);
 
 	private:
 	Surface* mySurface_;
@@ -44,7 +45,28 @@ void HW01Part2App::drawCircle(uint8_t* pixels, int center_x, int center_y, int r
 {
 	for(int y = center_y - radius; y <= center_y + radius; y++)
 	{
-		for(int x = center_x; x <= center_x + 2*radius; x++)
+		for(int x = center_x; x <= center_x + radius; x++)
+		{
+			//Bounds test, to make sure we don't access array out of bounds
+			if(y < 0 || x < 0 || x >= kAppWidth || y >= kAppHeight) continue;
+			int dist = (int)sqrt((double)((x-center_x)*(x-center_x) + (y-center_y)*(y-center_y)));
+			if(dist <= radius)
+			{
+					int offset = 3*(x + y*kTextureSize);
+					//By blending the colors I get a semi-transparent effect
+					pixels[offset] = color.r;
+					pixels[offset+1] = color.g;
+					pixels[offset+2] = color.b;
+			}
+		}
+	}
+}
+
+void HW01Part2App::drawSquare(uint8_t* pixels, int corner_x, int corner_y, int width, Color8u color)
+{
+	for(int y = corner_y; y <= corner_y + width; y++)
+	{
+		for(int x = corner_x; x <= corner_x + width; x++)
 		{
 			//Bounds test, to make sure we don't access array out of bounds
 			if(y < 0 || x < 0 || x >= kAppWidth || y >= kAppHeight) continue;
@@ -65,7 +87,9 @@ void HW01Part2App::mouseDown( MouseEvent event )
 void HW01Part2App::update()
 {
 	uint8_t* dataArray = (*mySurface_).getData();
-	drawCircle(dataArray, 400, 400, 50, Color8u(255,0,0));
+	drawCircle(dataArray, 200, 200, 50, Color8u(0,255,0));
+	drawSquare(dataArray, 400, 400, 50, Color8u(255,0,0));
+	drawCircle(dataArray, 450, 200, 100, Color8u(0,0,255));
 }
 
 void HW01Part2App::draw()
